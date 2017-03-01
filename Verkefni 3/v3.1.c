a36f21816f06347f8838cf8808dea07834f2265f
ingosibbason <ingolfursibbason@gmail.com>
2017-03-01 13:26:07 +0000

    v3.1
    KJóði fyrir 3.1

---
+++ Verkefni 3/v3.1.c
@@ -0,0 +1,53 @@
+#pragma config(Sensor, in1,    lightSensor,    sensorReflection)
+#pragma config(Sensor, in2,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, in3,    lineFollowerLeft, sensorLineFollower)
+#pragma config(Sensor, in4,    lineFollowerMid, sensorLineFollower)
+#pragma config(Sensor, in5,    lineFollowerRight, sensorLineFollower)
+#pragma config(Sensor, in6,    gyroSensor,     sensorGyro)
+#pragma config(Sensor, dgtl1,  rightEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  touchSensor,    sensorTouch)
+#pragma config(Sensor, dgtl6,  sonarIn,        sensorSONAR_mm)
+#pragma config(Sensor, dgtl8,  ,               sensorDigitalIn)
+#pragma config(Motor,  port2,           rightMotor,    tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorServoContinuousRotation, openLoop, reversed)
+#pragma config(Motor,  port4,           armMotor,      tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port5,           clawMotor,     tmotorServoContinuousRotation, openLoop)
+
+task main ()
+{
+
+	while(1 == 1)
+	{
+		motor[leftMotor]  = (vexRT[Ch2] + vexRT[Ch1])/2;  // (y + x)/2
+		motor[rightMotor] = (vexRT[Ch2] - vexRT[Ch1])/2;  // (y - x)/2
+
+// Raise, lower or do not move arm
+		if(vexRT[Btn5D] == 1)       	//If button 5U is pressed...
+		{
+			motor[armMotor] = 127;    	//...raise the arm.
+		}
+		else if(vexRT[Btn5U] == 1)  	//Else, if button 5D is pressed...
+		{
+			motor[armMotor] = -127;   	//...lower the arm.
+		}
+		else                      		//Else (neither button is pressed)...
+		{
+			motor[armMotor] = 0;      	//...stop the arm.
+		}
+
+// Open, close or do not more claw
+		if(vexRT[Btn6U] == 1)       	//If Button 6U is pressed...
+		{
+			motor[clawMotor] = 127;  		//...close the gripper.
+		}
+		else if(vexRT[Btn6D] == 1)  	//Else, if button 6D is pressed...
+		{
+			motor[clawMotor] = -127; 		//...open the gripper.
+		}
+		else                      		//Else (neither button is pressed)...
+		{
+			motor[clawMotor] = 0;    		//...stop the gripper.
+		}
+	}
+}
